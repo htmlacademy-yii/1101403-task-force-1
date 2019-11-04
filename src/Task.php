8f4437cde46862cf0d4e044fb1045a3060d33c8e
Анна <shesterikofff@mail.ru>
2019-11-04 13:07:55 +0300

    Изменила методы класса в соответствии с ТЗ, подключила автозагрузчик и написала тест

--- src/Task.php
+++ src/Task.php
@@ -1,26 +1,61 @@
 <?php
+namespace Htmlacademy;
 
 class Task
 {
-    // Константы:
-    const ACTIONS = [
-        'answer' => null,
-        'finish' => ['finished'],
-        'cancel' => ['cancelled'],
-        'refuse' => ['failed'],
-        'appoint' => ['in progress']
-    ];
-    const STATUSES = ['new', 'in progress', 'cancelled', 'failed', 'completed'];
-    const ROLES = ['client', 'executive'];
+    /**
+     * Константы возможных действий
+     */
+    const ACTION_ANSWER = 'answer';
+    const ACTION_COMPLETE = 'complete';
+    const ACTION_CANCEL = 'cancel';
+    const ACTION_REFUSE = 'refuse';
+    const ACTION_APPOINT = 'appoint';
 
-    // Свойства для хранения:
-    public $idClient = null;
-    public $idExecutive = null;
-    public $dtEnd = null;
-    public $statusActive = null;
+    /**
+     * Константы возможных статусов
+     */
+    const STATUS_NEW = ['' => 'new'];
+    const STATUS_COMPLETE = ['complete' => 'completed'];
+    const STATUS_CANCEL = ['cancel' => 'cancelled'];
+    const STATUS_REFUSE = ['refuse' => 'failed'];
+    const STATUS_APPOINT = ['appoint' => 'in progress'];
 
-    // Конструктор класса
-    public function __construct($idClient, $idExecutive, $dtEnd, $statusActive)
+    /**
+     * Константы возможных ролей
+     */
+    const ROLE_CLIENT = 'client';
+    const ROLE_EXECUTIVE = 'executive';
+
+    /**
+     * id заказчика
+     * @var int
+     */
+    public $idClient;
+    /**
+     * id исполнителя
+     * @var int
+     */
+    public $idExecutive;
+    /**
+     * Дата окончания существования задания
+     * @var string
+     */
+    public $dtEnd;
+    /**
+     * статус задания
+     * @var string
+     */
+    public $statusActive;
+
+    /**
+     * Конструктор класса Task
+     * @param $idClient
+     * @param $idExecutive
+     * @param $dtEnd
+     * @param string $statusActive
+     */
+    public function __construct($idClient, $idExecutive, $dtEnd, $statusActive = 'new')
     {
         $this->idClient = $idClient;
         $this->idExecutive = $idExecutive;
@@ -35,7 +70,7 @@ class Task
      */
     public function showActions(): array
     {
-        $actions = array_keys($this->ACTIONS);
+        $actions = [self::ACTION_ANSWER, self::ACTION_COMPLETE, self::ACTION_CANCEL, self::ACTION_REFUSE, self::ACTION_APPOINT];
         return $actions;
     }
 
@@ -45,26 +80,39 @@ class Task
      */
     public function showStatuses(): array
     {
-        return $this->STATUSES;
+        $statuses = [
+            self::STATUS_NEW[''],
+            self::STATUS_COMPLETE['complete'],
+            self::STATUS_CANCEL['cancel'],
+            self::STATUS_REFUSE['refuse'],
+            self::STATUS_APPOINT['appoint']
+        ];
+        return $statuses;
     }
 
     /** Возвращает статус, в который перейдет задача для указанного действия
      *
      * @param string $action - действие
      * @return string|null $statusNew
-    */
-    public function ifAction(string $action):? string
+     */
+    public function ifAction(string $action): string
     {
-        if (isset($this->ACTIONS[$action])) {
-            if ($this->ACTIONS[$action] === null) {
-                $statusNew = $this->statusActive;
-            } else {
-                $statusNew = $this->ACTIONS[$action];
+        $connections = [
+            self::STATUS_COMPLETE,
+            self::STATUS_CANCEL,
+            self::STATUS_REFUSE,
+            self::STATUS_APPOINT
+        ];
+        $statusNew = $this->statusActive;
+        foreach ($connections as $key => $value) {
+            foreach ($value as $act => $status) {
+                if ($action === $act) {
+                    $statusNew = $status;
+                }
             }
-        } else {
-            $statusNew = null;
         }
         return $statusNew;
     }
-
 }
+
+
