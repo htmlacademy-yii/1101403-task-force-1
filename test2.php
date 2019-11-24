fe452ed0a6ffad18fa07dd2a84339289b062b34d
Анна <shesterikofff@mail.ru>
2019-11-25 01:37:13 +0300

    Изменила тесты

--- test2.php
+++ test2.php
@@ -4,7 +4,7 @@ require_once('D:\HTML_academy\OSPanel\domains\1101403-task-force-1\vendor\autolo
 
 $task = new AvailableActions(63, 8, '30-12-2019');
 
-assert($task->getOpenActions('client', 63) === ['Htmlacademy\Logic\AppointAction','Htmlacademy\Logic\CancelAction']);
+assert($task->getOpenActions('client', 63) === ['appoint','cancel']);
 assert($task->getOpenActions('client', 5) === []);
-assert($task->getOpenActions('executive', 8) === ['Htmlacademy\Logic\ReplyAction']);
+assert($task->getOpenActions('executive', 8) === ['reply']);
 assert($task->getOpenActions('executive', 16) === []);
