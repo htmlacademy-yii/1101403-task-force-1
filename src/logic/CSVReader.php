5a39af6f548519d57646b7537b7466d67f48bc73
Анна <shesterikofff@mail.ru>
2019-12-22 20:38:25 +0300

    Создала класс для чтения из CSV файла

---
+++ src/logic/CSVReader.php
@@ -0,0 +1,84 @@
+<?php
+namespace Htmlacademy\Logic;
+
+use Htmlacademy\MyExceptions\FileFormatInvalid;
+
+class CSVReader
+{
+    /**
+     * @var $filePath - путь к файлу CSV
+     */
+    private $filePath;
+
+    /**
+     * @var - экземпляр класса \SplFileObject
+     */
+    private $file;
+
+    /**
+     * @var array массив с заголовками
+     */
+    private $headers;
+
+    /**
+     * CSVReader конструктор.
+     * @param $filePath - путь к файлу
+     * @throws FileFormatInvalid
+     */
+    public function __construct($filePath)
+    {
+        $this->filePath = $filePath;
+        $this->file = new \SplFileObject($this->filePath, 'r');
+        $this->headers = $this->getHeaderData();
+        if (!$this->getHeaderData()) {
+            throw new FileFormatInvalid('Заголовки полей заданы неверно');
+        }
+    }
+
+    /**
+     * Метод извлекает заголовки из файла CSV
+     * @return array массив с заголовками
+     */
+    protected function getHeaderData(): array
+    {
+        $this->file->rewind();
+        $headers = $this->file->fgetcsv();
+
+        return $headers;
+    }
+
+    /**
+     * Метод извлекает массив со следующей строкой файла CSV, если файл не подошел к концу
+     * @return array массив данных следующей строки
+     */
+    protected function getNextLine()
+    {
+        if (!$this->file->eof()) {
+            return $this->file->fgetcsv();
+        }
+    }
+
+    /**
+     * Метод возвращает двумерный массив с содержимым файла CSV
+     * @return array - двумерный массив вида [[массив_строки_1],[массив_строки_2],...].
+     */
+    public function getContent()
+    {
+        $content = [];
+        while($line = $this->getNextLine()) {
+            $content[] = $line;
+        }
+        return $content;
+    }
+
+    /**
+     * Метод возвращает заголовки файла CSV
+     * @return array - массив с заголовками
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+
+}
