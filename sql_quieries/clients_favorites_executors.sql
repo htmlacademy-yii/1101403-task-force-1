fa5a0daee6a540426484bd8379bf7b111abaa688
Анна <shesterikofff@mail.ru>
2019-12-22 20:57:00 +0300

    Сконвертированные sql файлы

---
+++ sql_quieries/clients_favorites_executors.sql
@@ -0,0 +1,6 @@
+USE taskforce;
+INSERT INTO clients_favorites_executors(id, users_client_id, users_executive_id) VALUES (1, '5', '2');
+INSERT INTO clients_favorites_executors(id, users_client_id, users_executive_id) VALUES (2, '3', '6');
+INSERT INTO clients_favorites_executors(id, users_client_id, users_executive_id) VALUES (3, '4', '2');
+INSERT INTO clients_favorites_executors(id, users_client_id, users_executive_id) VALUES (4, '7', '6');
+INSERT INTO clients_favorites_executors(id, users_client_id, users_executive_id) VALUES (5, '1', '4');
