669ccc125169dbe5138019eeecb4e05edafa2fa6
Анна <shesterikofff@mail.ru>
2020-01-08 20:09:59 +0300

    Файлы, сконвертированные из csv

---
+++ sql_quieries/task_replies.sql
@@ -0,0 +1,8 @@
+USE taskforce;
+INSERT INTO task_replies (id, executive_id, task_id, comment, price) VALUES (1, '10', '1', 'Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae', '5500' );
+INSERT INTO task_replies (id, executive_id, task_id, comment, price) VALUES (2, '3', '4', 'Fusce consequat. Nulla nisl. Nunc nisl.', '6800' );
+INSERT INTO task_replies (id, executive_id, task_id, comment, price) VALUES (3, '15', '9', 'In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat', '5500' );
+INSERT INTO task_replies (id, executive_id, task_id, comment, price) VALUES (4, '9', '1', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi', '4200' );
+INSERT INTO task_replies (id, executive_id, task_id, comment, price) VALUES (5, '4', '2', 'Praesent blandit. Nam nulla. Integer pede justo, lacinia eget', '3500' );
+INSERT INTO task_replies (id, executive_id, task_id, comment, price) VALUES (6, '8', '2', 'Morbi porttitor lorem id ligula. Suspendisse ornare consequat lectus.', '2500' );
+INSERT INTO task_replies (id, executive_id, task_id, comment, price) VALUES (7, '9', '10', 'In est risus, auctor sed, tristique in, tempus sit amet, sem.', '1599' );
---
+++ sql_quieries/users.sql
@@ -0,0 +1,16 @@
+USE taskforce;
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (1, '244', 'client', 'Andrew Binner', '1990-12-25', 'fdgdh@mail.ru', '89321587463', 'Hngfjncby3' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (2, '305', 'executive', 'Karrie Buttress', '2000-08-10', 'kbuttress0@1und1.de', '89115256354', 'JcfoKBYAB4k' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (3, '207', 'executive', 'Bob Aymer', '1978-12-21', 'baymer1@hp.com', '89202658874', 'ZEE54kg' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (4, '935', 'client', 'Zilvia Boulding', '1986-07-25', 'zboulding2@macromedia.com', '89096356585', 'VJyMV1Zat' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (5, '511', 'client', 'Emalee Mollon', '1993-11-13', 'emollon3@bloglovin.com', '89635254785', 'XUIeJ693h' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (6, '277', 'executive', 'Maria Mulberry', '1975-07-20', 'mmulberry4@cmu.edu', '89632524785', 'oWspnl' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (7, '970', 'executive', 'Levey By', '1988-02-12', 'lby5@mozilla.com', '89115459215', 'GdtcUU' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (8, '292', 'client', 'Baron Eates', '1968-05-03', 'beates6@last.fm', '89654223555', 'UQw6VeA' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (9, '1054', 'client', 'Trip Vink', '1972-01-13', 'tvink7@fotki.com', '89632587412', '49znXd7haFGz' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (10, '820', 'executive', 'Boonie Terbeck', '1985-09-15', 'bterbeck8@about.me', '85196354785', 'unCjJTF7sjs' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (11, '800', 'executive', 'Alonzo Traviss', '1989-09-25', 'atraviss9@auda.org.au', '89332289173', 'dLuVMAg' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (12, '401', 'client', 'Natassia Wittering', '1980-10-20', 'nwitteringa@google.com.br', '89632213161', 'tQlUG4n' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (13, '175', 'client', 'Felice Brooke', '1985-05-13', 'fbrookeb@nba.com', '89634567891', 's9y9Mcfgy1g' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (14, '210', 'client', 'Carlen Viccary', '1998-01-26', 'cviccaryc@amazon.co.uk', '89235587979', '9qd747vh' );
+INSERT INTO users (id, city_id, role, name, dt_birth, email, phone, password) VALUES (15, '524', 'executive', 'Hendrik Gethings', '1974-07-18', 'hgethingsd@sogou.com', '89632258745', 'zzN5c4' );
