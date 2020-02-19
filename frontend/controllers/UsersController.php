e809272b1a6e126809b7a72ef5a90bb1646372ba
Анна <shesterikofff@mail.ru>
2020-02-19 17:54:35 +0300

    Разделила код для читаемости

--- frontend/controllers/UsersController.php
+++ frontend/controllers/UsersController.php
@@ -8,7 +8,12 @@ class UsersController extends Controller
 {
     public function actionIndex()
     {
-        $users = Users::find()->select(['users.*', 'AVG(r.rate) AS rating', 'COUNT(t.id) AS exTasksNumber', 'COUNT(r.id) AS exReviewsNumber'])->where(['role' => 'executive'])->joinWith(['executivesTasks AS t', 'reviewsByExecutive AS r'])->groupBy('users.id')->orderBy(['dt_reg' => SORT_DESC])->all();
+        $users = Users::find()
+            ->select(['users.*', 'AVG(r.rate) AS rating', 'COUNT(t.id) AS exTasksNumber', 'COUNT(r.id) AS exReviewsNumber'])
+            ->where(['role' => 'executive'])
+            ->joinWith(['executivesTasks AS t', 'reviewsByExecutive AS r'])
+            ->groupBy('users.id')
+            ->orderBy(['dt_reg' => SORT_DESC])->all();
         return $this->render('index', ['users' => $users]);
     }
 }
