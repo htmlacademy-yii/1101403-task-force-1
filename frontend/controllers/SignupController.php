8eceb6cbd387ce082d964c80213579ab5f884e82
Анна <shesterikofff@mail.ru>
2020-05-18 19:45:30 +0300

    Использовала средства фреймворка для переадресации

--- frontend/controllers/SignupController.php
+++ frontend/controllers/SignupController.php
@@ -15,16 +15,15 @@ class SignupController extends Controller
         if (Yii::$app->request->isPost) {
             $model->load(Yii::$app->request->post());
             if ($model->validate()) {
-               $user = new Users();
-               $user->name = $model->username;
-               $user->email = $model->email;
-               $user->password = password_hash($model->password, PASSWORD_DEFAULT);
-               $user->city_id = !empty($model->chosenCity) ? $model->chosenCity : '';
-               $user->role = 'client';
-               $user->save();
-               //TO DO поменять роут на главную страницу
-               header('Location:/users');
-               exit();
+                $user = new Users();
+                $user->name = $model->username;
+                $user->email = $model->email;
+                $user->password = password_hash($model->password, PASSWORD_DEFAULT);
+                $user->city_id = !empty($model->chosenCity) ? $model->chosenCity : '';
+                $user->role = 'client';
+                $user->save();
+                //TO DO поменять роут на главную страницу
+                return $this->redirect(['/users']);
             }
             $errors = $model->getErrors();
         }
