d13de9d2209f314640967d3cf0d369d130b4c5de
Анна <shesterikofff@mail.ru>
2020-10-03 17:47:02 +0300

    Настроила показ откликов

--- frontend/views/tasks/view.php
+++ frontend/views/tasks/view.php
@@ -60,52 +60,70 @@ use yii\helpers\Url;
                   type="button" data-for="complete-form">Завершить</button>
         </div>
     </div>
-    <div class="content-view__feedback">
-        <h2>Отклики <span><?php echo '(' . count($task->taskReplies) . ')'; ?></span></h2>
-        <div class="content-view__feedback-wrapper">
-            <?php foreach ($task->taskReplies as $reply): ?>
-                <div class="content-view__feedback-card">
-                    <div class="feedback-card__top">
-                        <a href="<?php echo Url::toRoute(['users/view', 'id' => $reply->executive_id]); ?>"><img src="/img/man-glasses.jpg" width="55" height="55"></a>
-                        <div class="feedback-card__top--name">
-                            <p><a href="<?php echo Url::toRoute(['users/view', 'id' => $reply->executive_id]); ?>" class="link-regular"><?php echo $reply->executive->name ?: ''; ?></a></p>
-                            <?php
-                            $stars = intval(ceil($ratings[$reply->executive->id] ?: 0));
-                            if ($stars > 0) {
-                                for ($i = 0; $i < $stars; $i++) {
-                                    echo '<span></span>';
-                                }
-                            }
-                            $rest = 5 - $stars;
-                            if ($rest > 0) {
-                                for ($i = 0; $i < $rest; $i++) {
-                                    echo '<span class="star-disabled"></span>';
-                                }
-                            }
-                            ?>
-                           <b><?php echo $ratings[$reply->executive->id] ?: 0; ?></b>
-                        </div>
-                        <span class="new-task__time">
-                            <?php
-                            $counter = new TimeCounter($reply->dt_create);
-                            echo $counter->countTimePassed() ?: '';
-                            ?></span>
-                    </div>
-                    <div class="feedback-card__content">
-                        <p><?php echo $reply->comment ?: ''; ?></p>
-                        <span><?php echo $reply->price ?: ''; ?> ₽</span>
-                    </div>
-                    <div class="feedback-card__actions">
-                        <a class="button__small-color request-button button"
-                                type="button">Подтвердить</a>
-                        <a class="button__small-color refusal-button button"
-                                type="button">Отказать</a>
-                    </div>
-                </div>
-            <?php endforeach; ?>
+        <?php
+        $isAuthorOfReply = false;
+        $isTaskAuthor = false;
+        foreach($task->taskReplies as $reply) {
+            if ($reply->executive_id === Yii::$app->user->getId()) {
+                $isAuthorOfReply = true;
+            }
+        }
+        if ($task->client_id === Yii::$app->user->getId()) {
+            $isTaskAuthor = true;
+        }
+        ?>
+        <?php if(count($task->taskReplies) > 0 && ($isTaskAuthor or $isAuthorOfReply)): ?>
+            <div class="content-view__feedback">
+                <h2>Отклики <span><?php echo '(' . count($task->taskReplies) . ')'; ?></span></h2>
+                <div class="content-view__feedback-wrapper">
+                    <?php foreach ($task->taskReplies as $reply): ?>
+                        <?php if($isAuthorOfReply or $isTaskAuthor): ?>
+                            <div class="content-view__feedback-card">
+                                <div class="feedback-card__top">
+                                    <a href="<?php echo Url::toRoute(['users/view', 'id' => $reply->executive_id]); ?>"><img src="/img/man-glasses.jpg" width="55" height="55"></a>
+                                    <div class="feedback-card__top--name">
+                                        <p><a href="<?php echo Url::toRoute(['users/view', 'id' => $reply->executive_id]); ?>" class="link-regular"><?php echo $reply->executive->name ?: ''; ?></a></p>
+                                        <?php
+                                        $stars = intval(ceil($ratings[$reply->executive->id] ?: 0));
+                                        if ($stars > 0) {
+                                            for ($i = 0; $i < $stars; $i++) {
+                                                echo '<span></span>';
+                                            }
+                                        }
+                                        $rest = 5 - $stars;
+                                        if ($rest > 0) {
+                                            for ($i = 0; $i < $rest; $i++) {
+                                                echo '<span class="star-disabled"></span>';
+                                            }
+                                        }
+                                        ?>
+                                       <b><?php echo $ratings[$reply->executive->id] ?: 0; ?></b>
+                                    </div>
+                                    <span class="new-task__time">
+                                        <?php
+                                        $counter = new TimeCounter($reply->dt_create);
+                                        echo $counter->countTimePassed() ?: '';
+                                        ?></span>
+                                </div>
+                                <div class="feedback-card__content">
+                                    <p><?php echo $reply->comment ?: ''; ?></p>
+                                    <span><?php echo $reply->price ?: ''; ?> ₽</span>
+                                </div>
+                                <?php if($task->client_id === Yii::$app->user->getId()): ?>
+                                    <div class="feedback-card__actions">
+                                        <a class="button__small-color request-button button"
+                                                type="button">Подтвердить</a>
+                                        <a class="button__small-color refusal-button button"
+                                                type="button">Отказать</a>
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        <?php endif; ?>
+                    <?php endforeach; ?>
 
-        </div>
-    </div>
+                </div>
+            </div>
+        <?php endif; ?>
 </section>
 <section class="connect-desk">
     <div class="connect-desk__profile-mini">
